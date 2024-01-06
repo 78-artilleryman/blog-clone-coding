609d077325371886e523ad70fe345db3e7e8e17a
78-artilleryman <ywy040150@gmail.com>
2024-01-06 23:45:15 +0900

    dark mode update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Router from './components/Router';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { app } from 'firebaseApp';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Loader from 'components/Loader';
+import ThemeContext from 'context/ThemeContext';
 
 function App() {
+  const context = useContext(ThemeContext)
   const auth = getAuth(app);
   //auth를 체크하기 전에 (initialize 전)에는 loader를 뜨워주는 용도
   const [init, setInit] = useState<boolean>(false)
@@ -25,11 +27,11 @@ function App() {
     })
   },[auth])
   return (
-   <>
+   <div className={context.theme === "light" ? "white" : "dark"}>
     <ToastContainer/>
     {init ? <Router isAuthenticated={isAuthenticated}></Router> : <Loader/>}
     
-   </>
+   </div>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { BsSun ,BsMoonFill} from "react-icons/bs";
+import  ThemeContext from 'context/ThemeContext';
+
 
 function Footer() {
+  const context = useContext(ThemeContext)
+
   return (
     <>
        <footer>
         <Link to='/posts/new'>글쓰기</Link>
         <Link to='/posts'>게시글</Link>
         <Link to='/profile'>프로필</Link>
+        <div>
+          {context.theme === "light" ? (
+            <BsSun onClick={context.toggleMode} className='footer_theme_btn'/>
+          ) : (
+            <BsMoonFill onClick={context.toggleMode} className='footer_theme_btn'/>
+          )
+          }
+        </div>
       </footer>
     </>
   )
